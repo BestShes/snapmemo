cfb01ff708a2aaae370d64d0fcc7184a514f111f
BestShes <qkrdngus16@gmail.com>
2017-10-17 20:01:47 +0900

    AUTH_USER_MODEL 추가

--- snapmemo/snapmemo/settings.py
+++ snapmemo/snapmemo/settings.py
@@ -89,7 +89,7 @@ DATABASES = {
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
-
+AUTH_USER_MODEL = 'user.Member'
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
