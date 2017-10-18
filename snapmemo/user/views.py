5e837ab0f24d09dc14c5dec58e2c59f3507bb9e2
BestShes <qkrdngus16@gmail.com>
2017-10-18 14:17:38 +0900

    UserViewSet 기본 설정

--- snapmemo/user/views.py
+++ snapmemo/user/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework import permissions
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from user.models import Member
+from user.serializer import UserViewSetSerializer
+
+
+class UserViewSet(ModelViewSet):
+    queryset = Member.objects.all()
+    serializer_class = UserViewSetSerializer
+    lookup_field = 'username'
+    permission_classes = (permissions.AllowAny,)
