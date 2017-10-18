8e29e1ee64fe933d5b631b4a0c64dab8cc126c72
BestShes <qkrdngus16@gmail.com>
2017-10-18 17:54:13 +0900

    회원가입과 토큰 발행 구현

--- snapmemo/user/serializers.py
+++ snapmemo/user/serializers.py
@@ -4,15 +4,26 @@ from user.models import Member
 
 
 class UserViewSetSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(min_length=8, allow_blank=True, write_only=True)
-    access_key = serializers.CharField(allow_blank=True, write_only=True)
+    password = serializers.CharField(min_length=8, required=False, write_only=True)
+    access_key = serializers.CharField(required=False, write_only=True)
 
     class Meta:
         model = Member
         fields = (
             'username',
             'password',
+            'user_type',
             'access_key',
             'created_date',
         )
+
+    def create(self, validated_data):
+        user = Member(**validated_data)
+        try:
+            password = validated_data.pop('password')
+            user.set_password(password)
+        except KeyError:
+            pass
+        user.save()
+        return user
 
--- snapmemo/user/views.py
+++ snapmemo/user/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from rest_auth.app_settings import create_token
+from rest_auth.models import TokenModel
 from rest_framework import permissions
+from rest_framework import status
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
 from user.models import Member
@@ -11,3 +14,18 @@ class UserViewSet(ModelViewSet):
     serializer_class = UserViewSetSerializer
     lookup_field = 'username'
     permission_classes = (permissions.AllowAny,)
+
+    def perform_create(self, serializer):
+        user = serializer.save()
+        token = create_token(TokenModel, user, UserViewSetSerializer)
+        return token
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        token = self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(data={
+            'user': serializer.data,
+            'token': str(token)
+        }, status=status.HTTP_201_CREATED, headers=headers)
