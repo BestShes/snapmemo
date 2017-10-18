bc5abf69cc3d5f6274b90d481233b165432b6374
BestShes <qkrdngus16@gmail.com>
2017-10-18 22:06:18 +0900

    일반 유저 로그인 구현 토큰발행 포함

--- snapmemo/user/views.py
+++ snapmemo/user/views.py
@@ -2,11 +2,12 @@ from rest_auth.app_settings import create_token
 from rest_auth.models import TokenModel
 from rest_framework import permissions
 from rest_framework import status
+from rest_framework.mixins import CreateModelMixin
 from rest_framework.response import Response
-from rest_framework.viewsets import ModelViewSet
+from rest_framework.viewsets import ModelViewSet, GenericViewSet
 
 from user.models import Member
-from user.serializers import UserViewSetSerializer
+from user.serializers import UserViewSetSerializer, NormalUserLoginSerializer
 
 
 class UserViewSet(ModelViewSet):
@@ -29,3 +30,25 @@ class UserViewSet(ModelViewSet):
             'user': serializer.data,
             'token': str(token)
         }, status=status.HTTP_201_CREATED, headers=headers)
+
+
+class UserLoginViewSet(CreateModelMixin,
+                       GenericViewSet):
+    queryset = Member.objects.all()
+    serializer_class = NormalUserLoginSerializer
+    permission_classes = (permissions.AllowAny,)
+
+    def perform_create(self, serializer):
+        user = serializer.save()
+        token = create_token(TokenModel, user, NormalUserLoginSerializer)
+        return token
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        token = self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(data={
+            'user': serializer.data,
+            'token': str(token)
+        }, status=status.HTTP_200_OK, headers=headers)
