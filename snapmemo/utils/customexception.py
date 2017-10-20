3de1b436af7e38976429985ff56c06ce6a0728cc
BestShes <qkrdngus16@gmail.com>
2017-10-20 22:22:57 +0900

    Validation Exception 추가

--- snapmemo/utils/customexception.py
+++ snapmemo/utils/customexception.py
@@ -10,3 +10,8 @@ __all__ = (
 class AuthenticateException(APIException):
     status_code = status.HTTP_401_UNAUTHORIZED
     default_detail = _('Authentication credentials were not provided.')
+
+
+class ValidationException(APIException):
+    status_code = status.HTTP_400_BAD_REQUEST
+    default_detail = _('Validation Error')
