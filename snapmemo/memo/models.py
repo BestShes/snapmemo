250b9c90215cb34daa96a4db4716ce21058a0a1d
BestShes <qkrdngus16@gmail.com>
2017-10-21 14:10:35 +0900

    memo.models에 카테고리와 메모 클래스 추가, 이미지 필드를 사용하기 위한 Pillow설치 내용을 담은 requirements수정, memo 앱을 settings파일에 추가

--- snapmemo/memo/models.py
+++ snapmemo/memo/models.py
@@ -1,1 +1,15 @@
+from django.db import models
+from snapmemo import settings
 
+
+class Category(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL)
+    title = models.CharField(max_length=100)
+
+
+class Memo(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL)
+    category = models.ForeignKey(Category)
+    title = models.CharField(max_length=100)
+    content = models.CharField(max_length=250, blank=True)
+    image = models.ImageField(blank=True)
