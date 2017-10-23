afe7d28b9dacd33635d927623e6738ee23ed3c4e
BestShes <qkrdngus16@gmail.com>
2017-10-23 22:57:15 +0900

    카테고리는 대부분 완성, 메모는 리스트 부분만 완성

--- snapmemo/memo/views.py
+++ snapmemo/memo/views.py
@@ -1,3 +1,43 @@
-from django.shortcuts import render
+from rest_framework import permissions
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from memo.models import Category, Memo
+from memo.serializers import CategorySerializer, MemoSerializer
+from utils import CategoryPermission
+
+
+class CategoryViewSet(ModelViewSet):
+    serializer_class = CategorySerializer
+    queryset = Category.objects.all()
+    lookup_field = 'id'
+    permission_classes = (CategoryPermission,)
+
+    def list(self, request, *args, **kwargs):
+        user_id = request.user.id
+        queryset = Category.objects.filter(user_id=user_id)
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
+
+
+class MemoViewSet(ModelViewSet):
+    serializer_class = MemoSerializer
+    queryset = Memo.objects.all()
+    lookup_field = 'id'
+    permission_classes = (CategoryPermission,)
+
+    def list(self, request, *args, **kwargs):
+        user_id = request.user.id
+        queryset = Memo.objects.filter(user_id=user_id)
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(data={
+            'memo': serializer.data,
+        }, status=status.HTTP_201_CREATED, headers=headers)
