174fe9317ec006181e80a3e191bc1d9f53357684
BestShes <qkrdngus16@gmail.com>
2017-10-30 20:32:16 +0900

    카테고리와 메모 Serializer의 created_date, modified_date의 아웃풋 포맷을 YYYY-mm-dd HH:MM:SS로 변경

--- snapmemo/memo/serializers.py
+++ snapmemo/memo/serializers.py
@@ -7,6 +7,8 @@ from utils.customexception import ValidationException
 
 class MemoSerializer(ModelSerializer):
     category_id = serializers.IntegerField(required=False)
+    created_date = serializers.DateTimeField(format='%Y-%m-%d %H:%M:%S')
+    modified_date = serializers.DateTimeField(format='%Y-%m-%d %H:%M:%S')
 
     class Meta:
         model = Memo
@@ -38,6 +40,8 @@ class CategorySerializer(ModelSerializer):
     title = serializers.CharField(max_length=100, required=False)
     modify_title = serializers.CharField(max_length=100, write_only=True, required=False)
     memo_count = serializers.SerializerMethodField()
+    created_date = serializers.DateTimeField(format='%Y-%m-%d %H:%M:%S')
+    modified_date = serializers.DateTimeField(format='%Y-%m-%d %H:%M:%S')
 
     class Meta:
         model = Category
